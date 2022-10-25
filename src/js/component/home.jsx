485944f51eadac4dbb7d7942b4b59e47b56e7cc0
yasRF <yas070788@hotmail.es>
2022-10-25 16:02:20 +0000

    mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+	const Home = () =>{
+		const [total, setTotal]= useState(0);
+		const incrementar=()=>{
+			setTotal(total+1)
+		};
+			return(
+		
+			<div className="text-center  bg-dark">
+				<h1 className="text-white">{total}</h1>
+				<button onClick={incrementar}>INCREMENTAR</button>
+			</div>
+	
+		);
+	
+	};
+		
+	
+	
+	export default Home;
+	
