9cbaea780209983c2d78c93b885675826107b5cb
yasRF <yas070788@hotmail.es>
2022-11-20 14:31:56 +0000

    segundo commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,27 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-	const Home = () =>{
-		const [total, setTotal]= useState(0);
-		const incrementar=()=>{
-			setTotal(total+1)
-		};
-			return(
-		
-			<div className="text-center  bg-dark">
-				<h1 className="text-white">{total}</h1>
-				<button onClick={incrementar}>INCREMENTAR</button>
-			</div>
-	
-		);
-	
-	};
-		
-	
-	
-	export default Home;
-	
+const Home = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+  }, []);
+
+  return (
+    <div container="">
+      <h1>
+        Contador <div> {count}</div>
+      </h1>
+
+      <button onClick={() => setCount(0)}>RESET</button>
+    </div>
+  );
+};
+
+export default Home;
